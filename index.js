d2fd5e4e26cc506c94497a4d8e71a94aed2cf570
Wendy She <whshiwen@gmail.com>
2018-05-20 14:09:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+var musicians=[`John Lennon`,`Chopin`,`Joshua Bell`,`Kenny G`]
+var instruments=[`Guitar`,`Piano`,`Violin`,`Saxphone`]
+function theBeatlesPlay(array1,array2){var array=[];for(let i=0;i<array1.length;i++){array.push(`${array1[i]} plays ${array2[i]}`)};return array}
+const facts = [ "He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"]
+  
