1fc57947234ecf20dad0ec635809580704b8ffbc
Wendy She <whshiwen@gmail.com>
2018-05-20 14:17:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ function theBeatlesPlay(array1,array2){var array=[];for(let i=0;i<array1.length;
 const facts = [ "He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout",
   "He hated the sound of his own voice"]
   function johnLennonFacts(array) {var i=0; while(i<array.length){array[i]=`${array[i]}`+`!!!`;i++};return array}
+  var n=15
